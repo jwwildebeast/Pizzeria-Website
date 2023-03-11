7027c16aea629921cccac277eb522a37ef5027c5
John <jwwilli7309@gmail.com>
2023-03-10 22:54:57 -0500

    More changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+// import { useState } from 'react'
+// import { Routes, Route, useNavigate, Navigate } from 'react-router-dom'
+// import Nav from '../src/components/Nav'
+
+
+// const App = () => {
+
+
+//   return (
+//     <>
+//     <Nav />
+//       <Routes>
+//         <Route path="/" />
+//         <Route path="/menu" />
+//         <Route path="/coupons" />
+//         <Route path="/cart" />
+//         <Route path="/signup" />
+//         <Route path="/login" />
+//       </Routes>
+//     </>
+//   )
+// }
+
+// export default App;
+
+
+// import logo from './logo.svg';
 import './App.css';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.css'
+import NavBar from './components/Nav';
+import Menu from './pages/Menu';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Menu />
     </div>
   );
 }
